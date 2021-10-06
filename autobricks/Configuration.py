94c1227f254cd6b3408fec7803b9628036cbc29f
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-10-06 09:00:54 +0100

    feat: added json schema foc config

--- autobricks/Configuration.py
+++ autobricks/Configuration.py
@@ -5,6 +5,42 @@ Loads the configuration from environment variables.
 
 """
 
+_user_schema = {
+                    "type" : "object",
+                    "properties" : {
+                        "auth_type" : {"type" : "string"},
+                        "dbutilstoken" : {"type" : "string"},
+                        "databricks_api_host" : {"type" : "string"},
+                    },
+                }
+
+_sp_schema = {
+                    "type" : "object",
+                    "properties" : {
+                        "auth_type" : {"type" : "string"},
+                        "sp_client_id" : {"type" : "string"},
+                        "sp_client_secret" : {"type" : "string"},
+                        "ad_resource" : {"type" : "string"},
+                        "databricks_api_host" : {"type" : "string"},
+                    },
+                }
+
+_spme_schema = {
+                    "type" : "object",
+                    "properties" : {
+                        "auth_type" : {"type" : "string"},
+                        "sp_client_id" : {"type" : "string"},
+                        "sp_client_secret" : {"type" : "string"},
+                        "ad_resource" : {"type" : "string"},
+                        "tenant_id" : {"type" : "string"},
+                        "mgmt_resource_endpoint" : {"type" : "string"},
+                        "workspace_name" : {"type" : "string"},
+                        "resource_group" : {"type" : "string"},
+                        "subscription_id" : {"type" : "string"},
+                        "databricks_api_host" : {"type" : "string"},
+                    },
+                }
+
 config = {
     "dbutilstoken" : os.getenv("DBUTILSTOKEN"),
     "sp_client_id" : os.getenv("SP_CLIENT_ID"),
@@ -18,4 +54,3 @@ config = {
     "auth_type": os.getenv("AUTH_TYPE", "SERVICE_PRINCIPLE"),
     "databricks_api_host": os.getenv("DATABRICKS_API_HOST")
 }
-
